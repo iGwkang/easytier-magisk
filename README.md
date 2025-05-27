30f0831d40a657838429ceaf7cffe65439334436
Gwkang <975500206@qq.com>
2025-05-28 00:32:39 +0800

    更新2.3.0，同时修复peer无法解析域名

--- README.md
+++ README.md
@@ -2,12 +2,8 @@
 [EasyTier](https://github.com/EasyTier/EasyTier) for Magisk
 
 - 安装目录：`/data/adb/modules/easytier-magisk`
-- 配置文件：`/data/adb/easytier-magisk/config.conf`
+- 配置文件：`/data/adb/easytier-magisk/config.toml`
 
 修改配置文件后，点击magisk app中的`操作`按钮重新启动
 
 点击magisk app中的开关，停止运行easytier
-
-
-## bugs:
-1. peer无法使用域名，必须使用ip，比如: tcp://1.2.3.4:11010
--- easytier-magisk/customize.sh
+++ easytier-magisk/customize.sh
@@ -5,12 +5,12 @@ fi
 MODULE_ID = easytier-magisk
 
 if [ -f "/data/adb/easytier-magisk/config.yaml" ]; then
-    mv /data/adb/easytier-magisk/config.yaml /data/adb/easytier-magisk/config.conf
+    mv /data/adb/easytier-magisk/config.yaml /data/adb/easytier-magisk/config.toml
 fi
 
-if [ ! -f "/data/adb/easytier-magisk/config.conf" ]; then
+if [ ! -f "/data/adb/easytier-magisk/config.toml" ]; then
     mkdir -p /data/adb/easytier-magisk
-    cp -rf $MODPATH/config/config.conf /data/adb/easytier-magisk/config.conf
+    cp -rf $MODPATH/config/config.toml /data/adb/easytier-magisk/config.toml
 fi
 
 set_perm_recursive "$MODPATH/easytier" 0 0 0644 0755
@@ -22,5 +22,5 @@ set_perm "$MODPATH/run.sh" 0 0 0755
 ui_print "当前架构为: $ARCH"
 ui_print "当前系统版本为: $API"
 ui_print "安装目录为: /data/adb/modules/easytier-magisk"
-ui_print "配置文件位置: /data/adb/easytier-magisk/config.conf"
+ui_print "配置文件位置: /data/adb/easytier-magisk/config.toml"
 ui_print "修改后配置文件后在magisk app点击操作按钮即可生效"
